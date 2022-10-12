0c3c039ac5f17021ad56f3db1cb8a38477d02314
unknown <sazidahmednassir@gmail.com>
2022-10-12 10:42:31 -0700

    java type practice

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.net.SocketTimeoutException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.print("sum ");
+        int num1=4;
+        int num2=3;
+        int sum= num1+ num2;
+        System.out.println(sum);
     }
 }
